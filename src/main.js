81498e226b16ebc5bd24b76666bc508e275496b8
Manuel <dev@psicologica.ai>
2025-10-15 11:50:40 +0200

    tentativo autenticazione con auth0

--- src/main.js
+++ src/main.js
@@ -9,20 +9,20 @@ const app = createApp(App)
 
 // Configure Auth0
 // Determine the redirect URI based on environment
-const getRedirectUri = () => {
-  // Use production URL if on onslate.eu domain, otherwise use local
-  if (window.location.hostname.includes('onslate.eu')) {
-    return 'https://calciodomains-yqdhfgao.onslate.eu/auth-callback'
-  }
-  return `${window.location.origin}/auth-callback`
-}
+// const getRedirectUri = () => {
+//   // Use production URL if on onslate.eu domain, otherwise use local
+//   if (window.location.hostname.includes('onslate.eu')) {
+//     return 'https://calciodomains-yqdhfgao.onslate.eu/auth-callback'
+//   }
+//   return `${window.location.origin}/auth-callback`
+// }
 
 app.use(
   createAuth0({
     domain: "dev-giylww0unln6dunq.eu.auth0.com",
     clientId: "NIy1nFN8CeX42nrWAvB6jk2KNhNO77gG",
     authorizationParams: {
-      redirect_uri: getRedirectUri()
+      redirect_uri: 'https://calciodomains-yqdhfgao.onslate.eu/auth-callback'
     }
   })
 )
